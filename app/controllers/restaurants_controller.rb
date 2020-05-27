c407400dd800b2a69d0752465827dd1d552c9005
Clemence Nabet <clemence.nabet@kedgebs.com>
2020-05-27 13:44:26 +0200

    new & create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,31 @@
 class RestaurantsController < ApplicationController
+  # before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  private
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :address, :phone_number)
   end
 end
