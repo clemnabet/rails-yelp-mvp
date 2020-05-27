9b56874928b10db02dc76d0950f5d04c8c7e5fc5
Clemence Nabet <clemence.nabet@kedgebs.com>
2020-05-27 12:31:22 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# creating restaurants
+
+5.times do |_variable|
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code
+  )
+end
