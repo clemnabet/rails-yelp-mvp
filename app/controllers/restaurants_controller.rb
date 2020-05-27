72fa0bee43b6c715ae6550a382d17e6ab13b0907
Clemence Nabet <clemence.nabet@kedgebs.com>
2020-05-27 15:11:09 +0200

    done without design

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,18 +1,17 @@
 class RestaurantsController < ApplicationController
-  # before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
-
   def index
     @restaurants = Restaurant.all
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
-    redirect_to restaurants_path
+    redirect_to restaurant_path(@restaurant)
   end
 
   def new
@@ -20,10 +19,6 @@ class RestaurantsController < ApplicationController
   end
 
   private
-
-  # def set_restaurant
-  #   @restaurant = Restaurant.find(params[:id])
-  # end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :category, :address, :phone_number)
